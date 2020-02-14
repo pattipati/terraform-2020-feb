c588e8be690369a44a6509d9702436de04383d02
Rajani Patthipati <prajani2211@gmail.com>
2020-02-14 18:21:13 +0000

    terraform first commit

--- welcome.tf
+++ welcome.tf
@@ -1,7 +1,7 @@
 provider "aws"{
     region   = "ap-south-1"
-    access_key = "AKIAZLEEJAUQVZDPSF6R"
-    secret_key = "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"
+    access_key = "access_key"
+    secret_key = "secret_key"
 }
 
 // Create VPC
